707e495ee7523f99605eb8f6a8b9a9ea8f42e794
Artyma93 <artyma93@gmail.com>
2019-12-17 23:12:37 +0300

    Create
    Create project

---
+++ react-market/src/components/Header/Header.js
@@ -0,0 +1,15 @@
+import React from 'react'
+// import {AppBar, ToolBar, Typography} from "material-ui"
+import AppBar from "material-ui/AppBar"
+
+export default props => 
+<div>
+    <h1>Header</h1>
+</div>
+{/* <AppBar position="static">
+    <ToolBar>
+        <Typography variant="headline" color="inherit">
+                Header
+        </Typography>
+    </ToolBar>
+</AppBar> */}
