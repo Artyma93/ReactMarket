8ca7693e60025a78adcf5437d4cee1b618d83987
Artyma93 <artyma93@gmail.com>
2019-12-17 23:57:32 +0300

    Add Header and Container
    Add Header and Container

--- react-market/src/App.js
+++ react-market/src/App.js
@@ -1,9 +1,18 @@
 import React from 'react';
 import Header from "./components/Header/Header"
+import CssBaseline from '@material-ui/core/CssBaseline';
+import Typography from '@material-ui/core/Typography';
+import Container from '@material-ui/core/Container';
 
 function App() {
   return (
+    <div>
+      <Container maxWidth="xl">
       <Header/>
+        <Typography component="div" style={{ backgroundColor: '#cfe8fc', height: '100vh' }} />
+      </Container>
+
+    </div>
   );
 }
 
