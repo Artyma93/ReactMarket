a5871d81deb5e78057e46af406b5ff28fc284c35
Artyma93 <artyma93@gmail.com>
2020-01-08 22:04:53 +0300

    Добавили код
    Добавили код с сайта
    https://gist.github.com/nikneroz/c96d48563549abbe997abef5433fb111

--- react-market/src/App.js
+++ react-market/src/App.js
@@ -1,19 +1,85 @@
-import React from 'react';
-import Header from "./components/Header/Header"
-import CssBaseline from '@material-ui/core/CssBaseline';
-import Typography from '@material-ui/core/Typography';
-import Container from '@material-ui/core/Container';
+import React, { Component } from "react";
+import Header from "./components/Header/Header";
+import CssBaseline from "@material-ui/core/CssBaseline";
+import Typography from "@material-ui/core/Typography";
+import Container from "@material-ui/core/Container";
 
-function App() {
-  return (
-    <div>
-      <Container maxWidth="xl">
-      <Header/>
-        <Typography component="div" style={{ backgroundColor: '#cfe8fc', height: '100vh' }} />
-      </Container>
+import logo from "./logo.svg";
 
-    </div>
-  );
+import { connect } from "react-redux";
+
+class App extends Component {
+  render() {
+    const { fetching, dog, onRequestDog, error } = this.props;
+
+    return (
+      <div>
+        <Container maxWidth="xl">
+          <Header />
+          <Typography
+            component="div"
+            style={{ backgroundColor: "#cfe8fc", height: "100vh" }}
+          >
+            <div className="App">
+              <header className="App-header">
+                <img src={dog || logo} className="App-logo" alt="logo" />
+                <h1 className="App-title">Welcome to Dog Saga</h1>
+              </header>
+
+              {dog ? (
+                <p className="App-intro">Keep clicking for new dogs</p>
+              ) : (
+                <p className="App-intro">Replace the React icon with a dog!</p>
+              )}
+
+              {fetching ? (
+                <button disabled>Fetching...</button>
+              ) : (
+                <button onClick={onRequestDog}>Request a Dog</button>
+              )}
+
+              {error && (
+                <p style={{ color: "red" }}>Uh oh - something went wrong!</p>
+              )}
+            </div>
+          </Typography>
+        </Container>
+      </div>
+    );
+  }
 }
 
-export default App;
+const mapStateToProps = state => {
+  return {
+    fetching: state.fetching,
+    dog: state.dog,
+    error: state.error
+  };
+};
+
+const mapDispatchToProps = dispatch => {
+  return {
+    onRequestDog: () => dispatch({ type: "API_CALL_REQUEST" })
+  };
+};
+
+export default connect(mapStateToProps, mapDispatchToProps)(App);
+
+// function App() {
+//   const { fetching, dog, onRequestDog, error } = this.props;
+//   return (
+//     <div>
+//       <Container maxWidth="xl">
+//         <Header />
+//         <Typography
+//           component="div"
+//           style={{ backgroundColor: "#cfe8fc", height: "100vh" }}
+//         >
+//           asd
+//         </Typography>
+//       </Container>
+//     </div>
+//   );
+// }
+
+// export default App;
--- react-market/src/index.js
+++ react-market/src/index.js
@@ -1,8 +1,40 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
+import React from "react";
+import ReactDOM from "react-dom";
 
-import App from './App';
+// import App from './App';
 
+// ReactDOM.render(<App />, document.getElementById('root'));
 
-ReactDOM.render(<App />, document.getElementById('root'));
+import App from "./App";
+// import registerServiceWorker from "./registerServiceWorker";
 
+import { createStore, applyMiddleware, compose } from "redux";
+import createSagaMiddleware from "redux-saga";
+import { Provider } from "react-redux";
+
+import { reducer } from "./components/rootReducer";
+import { watcherSaga } from "./components/rootSaga";
+
+// create the saga middleware
+const sagaMiddleware = createSagaMiddleware();
+
+// dev tools middleware
+const reduxDevTools =
+  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__();
+
+// create a redux store with our reducer above and middleware
+let store = createStore(
+  reducer,
+  compose(applyMiddleware(sagaMiddleware), reduxDevTools)
+);
+
+// run the saga
+sagaMiddleware.run(watcherSaga);
+
+ReactDOM.render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.getElementById("root")
+);
+// registerServiceWorker();
