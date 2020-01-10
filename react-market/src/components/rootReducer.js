9ea6e38e51d4c6e02d09ea5c6699b00b15a29580
Artyma93 <artyma93@gmail.com>
2020-01-10 21:00:14 +0300

    Add Route

--- react-market/src/components/rootReducer.js
+++ react-market/src/components/rootReducer.js
@@ -1,7 +1,9 @@
-export const API_CALL_REQUEST = "API_CALL_REQUEST";
-export const API_CALL_SUCCESS = "API_CALL_SUCCESS";
-export const API_CALL_FAILURE = "API_CALL_FAILURE";
+// action types
+const API_CALL_REQUEST = "API_CALL_REQUEST";
+const API_CALL_SUCCESS = "API_CALL_SUCCESS";
+const API_CALL_FAILURE = "API_CALL_FAILURE";
 
+// reducer with initial state
 const initialState = {
   fetching: false,
   dog: null,
@@ -11,11 +13,14 @@ const initialState = {
 export function reducer(state = initialState, action) {
   switch (action.type) {
     case API_CALL_REQUEST:
-      return { ...state, fetching: true, dog: null, error: null };
+      return { ...state, fetching: true, error: null };
+      break;
     case API_CALL_SUCCESS:
-      return { ...state, fetching: false, dog: action.dog, error: null };
+      return { ...state, fetching: false, dog: action.dog };
+      break;
     case API_CALL_FAILURE:
       return { ...state, fetching: false, dog: null, error: action.error };
+      break;
     default:
       return state;
   }
--- react-market/src/index.js
+++ react-market/src/index.js
@@ -1,11 +1,10 @@
 import React from "react";
 import ReactDOM from "react-dom";
+// import "./index.css";
+import App from "./views/LandingPage/LangingPage";
+import Test1 from "./views/Test1Page/Test1";
+import Test2 from "./views/Test2Page/Test2";
 
-// import App from './App';
-
-// ReactDOM.render(<App />, document.getElementById('root'));
-
-import App from "./App";
 // import registerServiceWorker from "./registerServiceWorker";
 
 import { createStore, applyMiddleware, compose } from "redux";
@@ -15,25 +14,36 @@ import { Provider } from "react-redux";
 import { reducer } from "./components/rootReducer";
 import { watcherSaga } from "./components/rootSaga";
 
+import {
+  BrowserRouter as Router,
+  Route,
+  Switch,
+  Redirect
+} from "react-router-dom";
+
 // create the saga middleware
 const sagaMiddleware = createSagaMiddleware();
 
 // dev tools middleware
-const reduxDevTools =
-  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__();
+// const reduxDevTools =
+//   window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__();
 
 // create a redux store with our reducer above and middleware
-let store = createStore(
-  reducer,
-  compose(applyMiddleware(sagaMiddleware), reduxDevTools)
-);
+let store = createStore(reducer, compose(applyMiddleware(sagaMiddleware)));
 
 // run the saga
 sagaMiddleware.run(watcherSaga);
 
 ReactDOM.render(
   <Provider store={store}>
-    <App />
+    <Router>
+      <Switch>
+        <Route path="/" component={App} exact></Route>
+        <Route path="/test1" component={Test1} exact></Route>
+        <Route path="/test2" component={Test2} exact></Route>
+        <Redirect to="/" />
+      </Switch>
+    </Router>
   </Provider>,
   document.getElementById("root")
 );
